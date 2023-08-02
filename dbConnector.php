8d3bd1bd4972ea9813848e6a3fe561fa86bc8267
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-08-02 08:19:06 -0600

    DBConn Tweak
    Changed the port in DbConn

--- dbConnector.php
+++ dbConnector.php
@@ -12,7 +12,7 @@ function ConnGet()
 {
     // $dbConn will contain a resource link to the database
     // @ Don't display error
-    $dbConn = @mysqli_connect(DB_SERVER, DB_USER, DB_PSWD, DB_NAME, 3308)
+    $dbConn = @mysqli_connect(DB_SERVER, DB_USER, DB_PSWD, DB_NAME, 3306)
         or die('Failed to connect to MySQL ' . DB_SERVER . '::' . DB_NAME . ' : ' . mysqli_connect_error()); // Display messge and end PHP script
 
     return $dbConn;
