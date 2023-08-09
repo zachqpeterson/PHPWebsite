2686b6977cb3e2a95a6b27cc2f8e1672c37980f4
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-08-09 08:51:24 -0600

    About Page

--- About.php
+++ About.php
@@ -2,6 +2,38 @@
 include_once "MyHeader.php";
 ?>
 
+<h1>
+    About Us
+</h1>
+<p>
+    We are a small group of hostages being forced to develop a site in a language none of us like, using a database system that all of us hate. Please send help.
+    We are currently being held against our will at the Neumont College of Computer Science. Send the military, send the police, send Chuck Norris. We need help.
+    Included are a list of other acceptable heroes to help us in our time of need.
+</p>
+<br />
+<p>
+    Acceptable Heroes: <br />
+    Gandalf the Grey<br />
+    Gandalf the White<br />
+    Monty Python and the Holy Grail's Black Knight<br />
+    Benito Mussolini<br />
+    The Blue Meenie<br />
+    Cowboy Curtis<br />
+    Jambi the Genie<br />
+    Robocop<br />
+    The Terminator<br />
+    Captain Kirk<br />
+    Darth Vader<br />
+    Lo-Pan<br />
+    Superman<br />
+    Every Single Power Ranger<br />
+    Bill S. Preston and Theodore Logan<br />
+    Spock<br />
+    The Rock<br />
+    Doc Ock<br />
+    Hulk Hogan<br />
+
+</p>
 
 <?php
 include_once "MyFooter.php";
