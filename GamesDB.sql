81dc15bcda6a6d36bd0d48b9ef0e9021f3d20c6f
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-08-18 08:41:24 -0600

    Updated DB Create Script
    Now if you run the script it will include images.

--- GamesDB.sql
+++ GamesDB.sql
@@ -5,21 +5,22 @@ use GamesDB;
 CREATE TABLE SubPages(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     parentPage INT NULL,
+    image varchar(225) NULL,
     title VARCHAR(100),
     header VARCHAR(100),
     content VARCHAR(1000),
     isActive BOOL
 );
 
-INSERT INTO SubPages (parentPage, title, header, content, isActive) VALUES(NULL, "Genres", "All Genres", "Select a genre to view games of that genre", true);
-INSERT INTO SubPages (parentPage, title, header, content, isActive) VALUES(1, "RPG", "RPG Games", "These are games of the RPG variety", true);
-INSERT INTO SubPages (parentPage, title, header, content, isActive) VALUES(1, "Survival", "Survival Games", "These are games of the survival variety", true);
+INSERT INTO SubPages (parentPage, title, image, header, content, isActive) VALUES(NULL, "Genres", NULL, "All Genres", "Select a genre to view games of that genre", true);
+INSERT INTO SubPages (parentPage, title, image, header, content, isActive) VALUES(1, "RPG", NULL, "RPG Games", "These are games of the RPG variety", true);
+INSERT INTO SubPages (parentPage, title, image, header, content, isActive) VALUES(1, "Survival", NULL, "Survival Games", "These are games of the survival variety", true);
 
-INSERT INTO SubPages (parentPage, title, header, content, isActive) VALUES(2, "Baldur's Gate III", "Baldur's Gate III", "Baldur's Gate III is the hottest game of the year", true);
-INSERT INTO SubPages (parentPage, title, header, content, isActive) VALUES(2, "Baldur's Gate II", "Baldur's Gate II", "Baldur's Gate II is the hottest game of the year 2000", true);
-INSERT INTO SubPages (parentPage, title, header, content, isActive) VALUES(2, "Baldur's Gate", "Baldur's Gate", "Baldur's Gate is the hottest game forever ago", false);
+INSERT INTO SubPages (parentPage, title, image, header, content, isActive) VALUES(2, "Baldur's Gate III", "Images\\BaldursGateIII.png", "Baldur's Gate III", "Baldur's Gate III is the hottest game of the year", true);
+INSERT INTO SubPages (parentPage, title, image, header, content, isActive) VALUES(2, "Baldur's Gate II", "Images\\BaldursGateII.png","Baldur's Gate II", "Baldur's Gate II is the hottest game of the year 2000", true);
+INSERT INTO SubPages (parentPage, title, image, header, content, isActive) VALUES(2, "Baldur's Gate", "Images\\BaldursGate.png", "Baldur's Gate", "Baldur's Gate is the hottest game forever ago", false);
 
-INSERT INTO SubPages (parentPage, title, header, content, isActive) VALUES(3, "Minecraft", "Minecraft", "Minecraft is very overrated, but modded is cool I guess", true);
+INSERT INTO SubPages (parentPage, title, image, header, content, isActive) VALUES(3, "Minecraft", "Images\\Minecraft.png", "Minecraft", "Minecraft is very overrated, but modded is cool I guess", true);
 
 CREATE TABLE Users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -30,4 +31,3 @@ CREATE TABLE Users(
 );
 
 INSERT INTO Users (username, password, theme, admin) VALUES("ADMIN_USER", "ADMIN_PASSWORD", 1, true);
-INSERT INTO Users (username, password, theme, admin) VALUES("TEST_USER", "TEST_PASSWORD", 1, false);
