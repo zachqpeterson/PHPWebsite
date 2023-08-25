0d1517b960ebab9fc88617339ece6c9de3f45113
leland <lcarlson@student.neumont.edu>
2023-08-25 08:49:47 -0600

    tried to get the System theme to set as what the page loads as by default only worked half the time

--- MyHeader.php
+++ MyHeader.php
@@ -4,9 +4,33 @@ session_start();
 include_once "dbConnector.php";
 include_once "Helper.php";
 ?>
-
+<!--<script src="https://cdn.jsdelivr.net/npm/js-cookie/dist/js.cookie.min.js"></script>
+  <script>
+    // code to set the `color_scheme` cookie
+    var $color_scheme = Cookies.get("color_scheme");
+    function get_color_scheme() {
+      return (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
+    }
+    function update_color_scheme() {
+      Cookies.set("color_scheme", get_color_scheme());
+    }
+    // read & compare cookie `color-scheme`
+    if ((typeof $color_scheme === "undefined") || (get_color_scheme() != $color_scheme))
+      update_color_scheme();
+    // detect changes and change the cookie
+    if (window.matchMedia)
+      window.matchMedia("(prefers-color-scheme: dark)").addListener( update_color_scheme );
+  </script>-->
 <?php
-
+//$color_scheme = isset($_COOKIE["color_scheme"]) ? $_COOKIE["color_scheme"] : false;
+//if ($color_scheme === false) $color_scheme = 'light';
+//if ($color_scheme == 'dark') {
+//    setcookie("SelectedStyle", 1, time() + 3600);
+//    $PrefStyle = 1;
+//} else {
+//    setcookie("SelectedStyle", 2, time() + 3600);
+//    $PrefStyle = 2;
+//}
 if(array_key_exists('Login', $_POST))
 {
     if(isset($_SESSION['isAdmin']))
